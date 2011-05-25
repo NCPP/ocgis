274c34a17bdf8608fdc2229d139857cf9f634e49
Tyler Erickson <taericks@precisionm6500b.(none)>
2011-05-25 15:28:29 -0400

    modified the TEST_RUNNER setting so that the OCG tests can be run

--- src/openclimategis/settings.py
+++ src/openclimategis/settings.py
@@ -30,8 +30,11 @@ DATABASES = {
 }
 POSTGIS_VERSION = (1, 5, 2)
 POSTGIS_TEMPLATE = 'postgis-1.5.2-template'
-#TEST_RUNNER = 'django.test.simple.DjangoTestSuiteRunner'
-TEST_RUNNER = 'django.contrib.gis.tests.GeoDjangoTestSuiteRunner'
+
+# Use the GeoDjango Test Suite Runner to test the geospatial dependencies
+TEST_RUNNER = 'django.test.simple.DjangoTestSuiteRunner'
+#TEST_RUNNER = 'django.contrib.gis.tests.GeoDjangoTestSuiteRunner'
+
 #GEOS_LIBRARY_PATH = '/home/bob/local/lib/libgeos_c.so'
 #GDAL_LIBRARY_PATH = '/home/sue/local/lib/libgdal.so'
 

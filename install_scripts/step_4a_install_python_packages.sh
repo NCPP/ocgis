d77c12b7b6273576ee7512f682cd02d7ab0dfc0d
Tyler Erickson <taericks@precisionm6500b.(none)>
2011-06-21 10:18:12 -0400

    corrections to the install script

--- install_scripts/step_4a_install_python_packages.sh
+++ install_scripts/step_4a_install_python_packages.sh
@@ -6,12 +6,19 @@ STARTDIR=`pwd`
 echo "starting to install Python packages..."
 
 VIRTUALENVNAME=openclimategis
-mkvirtualenv --no-site-packages $VIRTUALENVNAME
 
+# create a python virtual environment
+curl -O https://raw.github.com/pypa/virtualenv/master/virtualenv.py
+python virtualenv.py --no-site-packages $VIRTUALENVNAME
+
+# install the OpenClimateGIS dependencies
+pip -E $VIRTUALENVNAME install yolk
 pip -E $VIRTUALENVNAME install Django>=1.3
 pip -E $VIRTUALENVNAME install psycopg2>=2.4
 pip -E $VIRTUALENVNAME install numpy>=1.5.1
 pip -E $VIRTUALENVNAME install netCDF4>=0.9.4
+
+# finally, install the OpenClimateGIS Django project
 pip -E $VIRTUALENVNAME install git+http://github.com/tylere/OpenClimateGIS
 
 echo "...finished installing Python packages"
--- install_scripts/step_5a_create_django_database.sh
+++ install_scripts/step_5a_create_django_database.sh
@@ -8,6 +8,8 @@ echo "starting to create a PostGIS database for Django..."
 
 # create a PostGIS database that will be used by the Django project
 DBNAME=openclimategis_sql
+DBOWNER=openclimategis_user
+sudo -u postgres createuser $DBOWNER --pwprompt
 sudo su -c "createdb $DBNAME -T $POSTGIS_TEMPLATE" - postgres
 sudo -u postgres psql -d postgres -c "ALTER DATABASE $DBNAME OWNER TO $DBOWNER;"
 

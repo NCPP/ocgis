80eccf237402dd9cc5d65fd06753e853d45d2be5
Tyler Erickson <taericks@precisionm6500b.(none)>
2011-10-06 12:40:31 -0700

    minor comment edits

--- src/openclimategis/settings.py
+++ src/openclimategis/settings.py
@@ -108,9 +108,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.gis',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     #'django_extensions', # enables extra django admin commands 
     'climatedata',

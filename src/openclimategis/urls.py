af6c5d8d1f737bbcbc9984ae5fa0698716d7c648
Tyler Erickson <taericks@precisionm6500b.(none)>
2011-11-29 14:07:29 -0800

    django serves media files when settings.DEBUG == True

--- src/openclimategis/urls.py
+++ src/openclimategis/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -21,3 +22,10 @@ urlpatterns = patterns('',
     # climate model archives index page
     (r'^archives|^arhives.html','climatedata.views.display_archives'),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )

0bd9d76915af3b70f40db1b59b776db64def613c
Arthur Endsley <kaendsle@mtu.edu>
2012-01-10 15:26:48 -0500

    Added more components; committing before deviation from storyboard where "Spatial" plane is concerned

--- src/openclimategis/media/js/builder.src.js
+++ src/openclimategis/media/js/builder.src.js
@@ -2,20 +2,66 @@ Ext.application({
     name: 'App',
     launch: function() {
     /////////////////////////////////////////////////// Application Entry Point
+        var viewport;
+        ///////////////////////////////////////////////////////////// Overrides
+        Ext.define('Override.form.field.ComboBox', {
+            override: 'Ext.form.field.ComboBox',
+            initialize : function() {
+                this.callOverridden(arguments);
+                },
+            labelWidth: 130
+            });
         /////////////////////////////////////////////////////////////// Classes
-        Ext.define('App.ui.MarkupComponent', {
+        Ext.define('App.ui.MarkupComponent', { // No ExtJS fluff
             extend: 'Ext.Component',
             alias: 'widget.markup',
             frame: false,
             border: 0
             }); // No callback (third argument)
-        Ext.define('App.ui.NestedPanel', {
+        Ext.define('App.ui.Container', { // Children should only be panels
             extend: 'Ext.Panel',
-            alias: 'widget.nested',
+            alias: 'widget.container',
             resizable: true
             }); // No callback (third argument)
+        Ext.define('App.ui.NestedPanel', { // Padded bodies
+            extend: 'Ext.Panel',
+            alias: 'widget.nested',
+            resizable: true,
+            bodyPadding: 7
+            }); // No callback (third argument)
+        Ext.define('App.ui.MapPanel', { // Only for the map
+            extend: 'Ext.Panel',
+            alias: 'widget.map',
+            resizable: true
+            }); // No callback (third argument)
+        Ext.define('App.ui.DateRange', {
+            extend: 'Ext.form.FieldContainer',
+            alias: 'widget.daterange',
+            msgTarget: 'side',
+            layout: 'hbox',
+            labelWidth: 90,
+            width: 290,
+            defaults: {
+                width: 90,
+                hideLabel: true
+                },
+            items: [
+                {
+                    xtype: 'datefield',
+                    name: 'startDate',
+                    itemId: 'date-start',
+                    margin: '0 5 0 0'
+                    },
+                {
+                    xtype: 'datefield',
+                    name: 'endDate',
+                    itemId: 'date-end'
+                    }
+                ]
+            }); // No callback (third argument)
         //////////////////////////////////////////////////////////// Components
-        Ext.create('Ext.container.Viewport', { // Viewport
+        viewport = Ext.create('Ext.container.Viewport', { // Viewport
+            id: 'viewport',
             layout: 'border',
             items: [
                 { // Banner
@@ -29,10 +75,13 @@ Ext.application({
                     },
                 { // Form
                     xtype: 'form',
+                    id: 'form-panel',
                     region: 'center',
                     layout: 'border',
                     items: [
                         {
+                            xtype: 'container',
+                            itemId: 'form-sidebar',
                             region: 'west',
                             width: 300,
                             border: 0,
@@ -40,17 +89,20 @@ Ext.application({
                             items: [
                                 { // Data selection
                                     xtype: 'nested',
+                                    itemId: 'form-data-sel',
                                     title: 'Data Selection',
                                     region: 'north',
                                     height: 200
                                     },
                                 { // Temporal selection
                                     xtype: 'nested',
-                                    title: 'Temporal Selection',
+                                    itemId: 'form-time-sel',
+                                    title: 'Temporal',
                                     region: 'center'
                                     },
                                 { // Output format
                                     xtype: 'nested',
+                                    itemId: 'form-output',
                                     title: 'Output Format',
                                     region: 'south',
                                     height: 100
@@ -58,11 +110,43 @@ Ext.application({
                                 ]
                             },
                         { // Spatial selection
-                            title: 'Spatial Selection',
-                            region: 'center'
+                            xtype: 'map',
+                            title: 'Spatial',
+                            region: 'center',
+                            tbar: {
+                                defaults: {
+                                    style: {fontSize: '11px'}
+                                    },
+                                items: [
+                                    {
+                                        xtype: 'combo',
+                                        fieldLabel: 'Area-of-Interest (AOI)',
+                                        emptyText: '(None Selected)'
+                                        },
+                                    ' ', // Spacer
+                                    {
+                                        xtype: 'button',
+                                        text: 'Manage AOIs',
+                                        iconCls: 'icon-app-edit'
+                                        },
+                                    '-', // Vertical separator
+                                    ' ', // Spacer
+                                    {
+                                        xtype: 'checkbox',
+                                        boxLabel: 'Clip Output to AOI'
+                                        },
+                                    ' ', // Spacer
+                                    '-', // Vertical separator
+                                    ' ', // Spacer
+                                    {
+                                        xtype: 'checkbox',
+                                        boxLabel: 'Aggregate Geometries'
+                                        }
+                                    ] // eo items
+                                } // eo tbar
                             },
                         { // Data request URL
-                            xtype: 'panel',
+                            xtype: 'nested',
                             region: 'south',
                             height: 150,
                             title: 'Data Request URL',
@@ -80,6 +164,57 @@ Ext.application({
                     },
                 ] // eo items
             }); // eo Ext.create
+        // Add items to the Data Selection panel ///////////////////////////////
+        (function() {
+            var p = Ext.getCmp('form-panel').getComponent('form-sidebar').getComponent('form-data-sel');
+            p.add([
+                {
+                    xtype: 'combo',
+                    fieldLabel: 'Archive'
+                    },
+                {
+                    xtype: 'combo',
+                    fieldLabel: 'Emissions Scenario'
+                    },
+                {
+                    xtype: 'combo',
+                    fieldLabel: 'Climate Model'
+                    },
+                {
+                    xtype: 'combo',
+                    fieldLabel: 'Variable'
+                    },
+                {
+                    xtype: 'combo',
+                    fieldLabel: 'Run'
+                    },
+                {
+                    xtype: 'daterange',
+                    fieldLabel: 'Date Range'
+                    },
+                ]);
+            }()); // Execute immediately
+        // Add items to the Temporal Selection panel ///////////////////////////
+        (function() {
+            var p = Ext.getCmp('form-panel').getComponent('form-sidebar').getComponent('form-time-sel');
+            p.add([
+                {
+                    xtype: 'combo',
+                    fieldLabel: 'Grouping Interval'
+                    }
+                ]);
+            }()); // Execute immediately
+        // Add items to the Output Format panel ////////////////////////////////
+        (function() {
+            var p = Ext.getCmp('form-panel').getComponent('form-sidebar').getComponent('form-output');
+            p.add([
+                {
+                    xtype: 'combo',
+                    labelAlign: 'top',
+                    fieldLabel: 'Output Format'
+                    }
+                ]);
+            }()) // Execute immediately
         ////////////////////////////////////////////////////////////////////////
         } // eo launch()
     }); // eo Ext.application

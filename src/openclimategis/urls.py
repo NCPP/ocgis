0779ea0212d3c025914d050b3b19ebfc7ce3ec49
Tyler Erickson <taericks@precisionm6500b.(none)>
2012-01-17 11:53:43 -0800

    updated config to allow the django development server to access Apache static files

--- src/openclimategis/urls.py
+++ src/openclimategis/urls.py
@@ -25,4 +25,7 @@ if settings.DEBUG:
         url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': settings.MEDIA_ROOT,
         }),
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.APACHE_STATIC_ROOT,
+        }),
    )

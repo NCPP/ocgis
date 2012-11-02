bd63c0c7df0ff310e15741dd86636021440855b0
Ben Koziol <benkoziol@gmail.com>
2012-11-02 14:39:48 -0600

    before starting modifications to handle point abstraction error.

--- src/django/openclimategis/cdata/tests.py
+++ src/django/openclimategis/cdata/tests.py
@@ -72,6 +72,11 @@ class TestCdata(TestCase):
 #                          'year|month'
                           ]
         
+        s_abstractions = [
+#                          'polygon',
+                          'point'
+                          ]
+        
 #        calc = ''
 #        calc = ('&calc=max~max|min~min&calc_raw=false&calc_grouping=month')
 #        calc = ('&calc='
@@ -85,8 +90,8 @@ class TestCdata(TestCase):
                 kv = '&'+kv
             return(url+kv)
 
-        args = (spaces,datasets,outputs,times,levels,operations,aggregates,calcs,calc_raws,calc_groupings)
-        for space,dataset,output,time,level,operation,agg,calc,calc_raw,calc_grouping in itertools.product(*args):
+        args = (spaces,datasets,outputs,times,levels,operations,aggregates,calcs,calc_raws,calc_groupings,s_abstractions)
+        for space,dataset,output,time,level,operation,agg,calc,calc_raw,calc_grouping,s_abstraction in itertools.product(*args):
             dataset[0] = str(dataset[0])
             if '|' not in dataset[0] and 'heat_index' in calc:
                 continue
@@ -105,6 +110,7 @@ class TestCdata(TestCase):
             url = _append_(url,'calc',calc)
             url = _append_(url,'calc_raw',calc_raw)
             url = _append_(url,'calc_grouping',calc_grouping)
+            url = _append_(url,'s_abstraction',s_abstraction)
             
 #            try:
             resp = self.c.get(url)

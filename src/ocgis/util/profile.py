449e447b4ea1cfac05ed096c1c7b8e33a1546297
Ben Koziol <benkoziol@gmail.com>
2012-11-19 11:40:01 -0700

    added basic profiling script.

---
+++ src/ocgis/util/profile.py
@@ -0,0 +1,22 @@
+import cProfile
+from ocgis.api.operations import OcgOperations
+from ocgis.api.iocg.interpreter_ocg import OcgInterpreter
+import pstats
+
+
+def run():
+    dataset = {'uri':'/usr/local/climate_data/CanCM4/tasmax_day_CanCM4_decadal2000_r2i1p1_20010101-20101231.nc','variable':'tasmax'}
+    ops = OcgOperations(dataset=dataset,snippet=True)
+    ret = OcgInterpreter(ops).execute()
+    
+    
+def main():
+    path = '/tmp/stats.txt'
+    cProfile.run('run()',path)
+    p = pstats.Stats(path)
+    p.sort_stats('time').print_stats(30)
+    import ipdb;ipdb.set_trace()
+    
+    
+if __name__ == '__main__':
+    main()

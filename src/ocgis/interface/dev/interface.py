f032a279b0244c5983b9be1259f1a28e2c73d22f
Ben Koziol <benkoziol@gmail.com>
2012-12-10 09:57:10 -0700

    start of day.

--- src/ocgis/interface/dev/interface.py
+++ src/ocgis/interface/dev/interface.py
@@ -22,9 +22,20 @@ class Interface(object):
                 except AttributeError:
                     warn('guessing dimension location with "axis" attribute missing')
                     axis = self._guess_by_location_(dims,dim)
-                mp[axis] = dimvar
+                mp[axis] = {'variable':dimvar}
             except KeyError:
                 raise(NotImplementedError)
+            
+        ## look for bounds variables
+        bounds_names = set(['bounds','bnds'])
+        for key,value in mp.iteritems():
+            if value is None:
+                continue
+            bounds_var = None
+            var = value['variable']
+            intersection = list(bounds_names.intersection(set(var.ncattrs())))
+            bounds_var = rootgrp.variables[getattr(var,intersection[0])]
+            value.update({'bounds':bounds_var})
         return(mp)
             
     def _guess_by_location_(self,dims,target):

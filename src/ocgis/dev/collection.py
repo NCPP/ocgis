e7d4a0b2a80c5434949843b1eb6e709f4eb94488
Ben Koziol <benkoziol@gmail.com>
2012-12-14 09:24:06 -0700

    dev work on collections. working timegroup dimension.

--- src/ocgis/dev/collection.py
+++ src/ocgis/dev/collection.py
@@ -104,56 +104,52 @@ class TemporalDimension(OcgDimension):
     
     def _group_part_(self,groups):
         
-        date_parts = ('year','month','day','hour','minute','second',)
+        date_parts = ('year','month','day','hour','minute','second','microsecond')
         group_map = dict(zip(range(0,7),date_parts,))
         group_map_rev = dict(zip(date_parts,range(0,7),))
-        
-        if self.bounds is None:
-            vrshp = self.value.reshape(-1,1)
-            dval = np.hstack((vrshp,vrshp))
-        else:
-            dval = self.bounds.copy()
-            delta = datetime.timedelta(microseconds=1)
-            for idx in range(dval.shape[0]):
-                dval[idx,1] = dval[idx,1]-delta
+        value = self.value
 
         def _get_attrs_(dt):
-            return([dt.year,dt.month,dt.day,dt.hour,dt.minute,dt.second,])
-        parts = np.empty((len(self.value),2,len(date_parts)),dtype=int)
-        for row,col in itertools.product(range(parts.shape[0]),range(parts.shape[1])):
-            parts[row,col,:] = _get_attrs_(dval[row,col])
+            return([dt.year,dt.month,dt.day,dt.hour,dt.minute,dt.second,dt.microsecond])
+        parts = np.empty((len(self.value),len(date_parts)),dtype=int)
+        for row in range(parts.shape[0]):
+            parts[row,:] = _get_attrs_(value[row])
         
         unique = deque()
-        for idx in range(parts.shape[2]):
+        for idx in range(parts.shape[1]):
             if group_map[idx] in groups:
-                fill = np.unique(parts[:,:,idx])
+                fill = np.unique(parts[:,idx])
             else:
                 fill = np.array([0])
             unique.append(fill)
-        
+
         select = deque()
         idx2_seq = range(len(date_parts))
         for idx in itertools.product(*[range(len(u)) for u in unique]):
             select.append([unique[idx2][idx[idx2]] for idx2 in idx2_seq])
         select = np.array(select)
-
+        
         dgroups = deque()
         idx_cmp = [group_map_rev[group] for group in groups]
+        keep_select = []
         for idx in range(select.shape[0]):
-            match = select[idx,idx_cmp] == parts[:,:,idx_cmp]
-            dgrp = np.any(np.all(match,axis=2),axis=1)
+            match = select[idx,idx_cmp] == parts[:,idx_cmp]
+            dgrp = match.all(axis=1)
             if dgrp.any():
+                keep_select.append(idx)
                 dgroups.append(dgrp)
-            
+        select = select[keep_select,:]
+        assert(len(dgroups) == select.shape[0])
+
         new_value = np.empty((len(dgroups),len(date_parts)),dtype=int)
         new_bounds = np.empty((len(dgroups),2),dtype=object)
         if self.bounds is None:
-            bounds = dval
+            bounds = value
         else:
             bounds = self.bounds
         for idx,dgrp in enumerate(dgroups):
-            sel = bounds[dgrp]
             new_value[idx] = select[idx]
+            sel = bounds[dgrp,:]
             new_bounds[idx,:] = [sel.min(),sel.max()]
 
         return(new_value,new_bounds,dgroups)
--- src/ocgis/dev/test_collection.py
+++ src/ocgis/dev/test_collection.py
@@ -34,7 +34,6 @@ class TestCollection(unittest.TestCase):
         oid.add(56)
         self.assertEqual(oid,{55:1,56:2})
         
-        
     def get_TemporalDimension(self,add_bounds=True):
         start = datetime.datetime(2000,1,1,12)
         end = datetime.datetime(2001,12,31,12)
@@ -61,25 +60,83 @@ class TestCollection(unittest.TestCase):
         
         return(tdim)
     
+    def get_hourly_TemporalDimension(self,add_bounds=True):
+        start = datetime.datetime(2000,1,1,0,30)
+        end = datetime.datetime(2001,12,31,23,30)
+        delta = datetime.timedelta(hours=1)
+        times = []
+        check = start
+        while check <= end:
+            times.append(check)
+            check += delta
+        times = np.array(times)
+        
+        if add_bounds:
+            time_bounds = []
+            delta = datetime.timedelta(minutes=30)
+            for t in times.flat:
+                time_bounds.append([t-delta,t+delta])
+            time_bounds = np.array(time_bounds)
+        else:
+            time_bounds = None
+        
+        uid = np.arange(1,len(times)+1)
+        
+        tdim = TemporalDimension(uid,times,bounds=time_bounds)
+
+        return(tdim)
+    
+    def get_monthly_TemporalDimension(self,add_bounds=True):
+        years = [2000,2001]
+        months = range(1,13)
+        times = np.empty(len(years)*len(months),dtype=object)
+        time_bounds = np.empty((len(times),2),dtype=object)
+        for idx,(year,month) in enumerate(itertools.product(years,months)):
+            times[idx] = datetime.datetime(year,month,15)
+            time_bounds[idx,0] = datetime.datetime(year,month,1)
+            try:
+                time_bounds[idx,1] = datetime.datetime(year,month+1,1)
+            except ValueError:
+                time_bounds[idx,1] = datetime.datetime(year+1,1,1)
+        
+        if not add_bounds:
+            time_bounds = None
+        
+        uid = np.arange(1,len(times)+1)
+        
+        tdim = TemporalDimension(uid,times,bounds=time_bounds)
+        
+        return(tdim)
+    
     def test_TemporalGroupDimension(self):
         
-#        perms = ['year','month','day','hour']
-        perms = ['hour']
-#        add_bounds_opts = [True,False]
-        add_bounds_opts = [True]
-        for ii,add_bounds in itertools.product(range(1,4),add_bounds_opts):
+        tdims = [
+#                 self.get_hourly_TemporalDimension,
+                 self.get_TemporalDimension,
+                 self.get_monthly_TemporalDimension,
+                 ]
+        perms = ['year','month','day','hour','minute','second','microsecond']
+#        perms = [['day','hour']]
+        add_bounds_opts = [True,False]
+#        add_bounds_opts = [True]
+        for ii,add_bounds,tdim_func in itertools.product(range(1,4),add_bounds_opts,tdims):
             for perm in itertools.permutations(perms,ii):
-                print perm
-                tdim = self.get_TemporalDimension(add_bounds=add_bounds)
-                tgdim = tdim.group(perm)
+                tdim = tdim_func(add_bounds=add_bounds)
+                try:
+                    tgdim = tdim.group(perm)
+                except TypeError:
+                    tgdim = tdim.group(*perm)
                 for row in tgdim.iter_rows():
-                    if False and np.random.rand() <= 0.25 and add_bounds is False:
-                        print perm
+                    if np.random.rand() <= 0.01:
+                        print (perm,add_bounds)
                         print row
                         import ipdb;ipdb.set_trace()
                     else:
                         continue
+#                try:
                 self.assertEqual(np.sum([dgrp.sum() for dgrp in tgdim.dgroups]),len(tdim.value))
+#                except:
+#                    import ipdb;ipdb.set_trace()
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']

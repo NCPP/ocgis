ee596a38564ce6fbb6fe58b997b807a0df16ba89
Ben Koziol <benkoziol@gmail.com>
2012-12-19 10:53:04 -0700

    working ShpIdxConverter

--- src/ocgis/conv/shpidx.py
+++ src/ocgis/conv/shpidx.py
@@ -1,20 +1,12 @@
-from ocgis.util.helpers import iter_array
 from ocgis.conv.shp import ShpConverter
-from ocgis.api.dataset.collection.collection import Identifier
 
     
 class ShpIdxConverter(ShpConverter):
     
-    def __init__(self,*args,**kwds):
-        super(self.__class__,self).__init__(*args,**kwds)
-        
-        self._id = Identifier()
-    
-    def get_headers(self):
+    def get_headers(self,coll):
         return(['UGID'])
     
     def get_iter(self,coll):
-        import ipdb;ipdb.set_trace()
-        for gidx,gid in iter_array(coll.gid,return_value=True):
-            geom,area_km2 = self._process_geom_(coll.geom[gidx])
-            yield([gid],geom)
+        arch = coll._arch
+        for row in arch.spatial:
+            yield([row[1]['gid']],row[1]['geom'])

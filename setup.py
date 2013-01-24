6b3bfaa50f594dc2de507cb59e14ac784cbf84a2
Ben Koziol <benkoziol@gmail.com>
2013-01-24 14:02:52 -0700

    added install and uninstall flags to setup.py. uninstall command currently does not work.

--- setup.py
+++ setup.py
@@ -2,27 +2,41 @@ from distutils.core import setup
 import sys
 
 
-## check python version
-version = float(sys.version_info.major) + float(sys.version_info.minor)/10
-if version < 2.7 or version > 2.7:
-    raise(ImportError('This software requires Python version 2.7.'))
+try:
+    arg = sys.argv[1]
+except IndexError:
+    raise(ValueError('Please supply an "install" or "uninstall" command.'))
 
-## attempt package imports
-pkgs = ['numpy','netCDF4','osgeo','shapely']
-for pkg in pkgs:
+if arg == 'install':
+    ## check python version
+    version = float(sys.version_info.major) + float(sys.version_info.minor)/10
+    if version < 2.7 or version > 2.7:
+        raise(ImportError('This software requires Python version 2.7.'))
+    
+    ## attempt package imports
+    pkgs = ['numpy','netCDF4','osgeo','shapely']
+    for pkg in pkgs:
+        try:
+            __import__(pkg)
+        except ImportError:
+            msg = 'Unable to import Python package: "{0}".'.format(pkg)
+            raise(ImportError(msg))
+    
+    setup(name='ocgis',
+          version='0.03a',
+          author='Ben Koziol',
+          author_email='ben.koziol@noaa.gov',
+          url='https://github.com/NCPP/ocgis/tags',
+          license='BSD License',
+          platforms=['all'],
+          packages=['ocgis'],
+          package_dir = {'': 'src'}
+          )
+elif arg == 'uninstall':
     try:
-        __import__(pkg)
+        import ocgis
+        raise(NotImplementedError)
     except ImportError:
-        msg = 'Unable to import Python package: "{0}".'.format(pkg)
-        raise(ImportError(msg))
-
-setup(name='ocgis',
-      version='0.03a',
-      author='Ben Koziol',
-      author_email='ben.koziol@noaa.gov',
-      url='https://github.com/NCPP/ocgis/tags',
-      license='BSD License',
-      platforms=['all'],
-      packages=['ocgis'],
-      package_dir = {'': 'src'}
-      )
+        raise(ImportError("Either OpenClimateGIS is not installed or not available on the Python PATH."))
+else:
+    raise(ValueError('Only "install" and "uninstall" commands are supported. Command not recognized: "{0}".'.format(arg)))

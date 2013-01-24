1d849e00aeb5c41c1459d498bd7ecf3aecd0b470
Ben Koziol <benkoziol@gmail.com>
2013-01-24 10:22:48 -0700

    modifications to examples to work with new requestdataset objects.

--- examples/examples.py
+++ examples/examples.py
@@ -1,12 +1,22 @@
-from ocgis import OcgOperations, ShpCabinet
+import ocgis
+import os.path
 
 
-DATASET = {'uri':'/usr/local/climate_data/CanCM4/tasmax_day_CanCM4_decadal2000_r2i1p1_20010101-20101231.nc',
-           'variable':'tasmax'}
+CLIMATE_DATA_DIRECTORY = '/usr/local/climate_data/CanCM4'
+NC1 = 'tasmax_day_CanCM4_decadal2000_r2i1p1_20010101-20101231.nc'
+VAR1 = 'tasmax'
+NC2 = 'tasmin_day_CanCM4_decadal2000_r2i1p1_20010101-20101231.nc'
+VAR2 = 'tasmin'
 
+def make_uri(filename):
+    return(os.path.join(CLIMATE_DATA_DIRECTORY,filename))
 
-## subset data by the boundaries of the USA
-ops = OcgOperations(dataset=DATASET,geom='state_boundaries',agg_selection=True)
+rd1 = ocgis.RequestDataset(make_uri(NC1),VAR1)
+rd2 = ocgis.RequestDataset(make_uri(NC2),VAR2)
+
+## subset data by the boundaries of the USA ####################################
+
+ops = ocgis.OcgOperations(dataset=rd1,geom='state_boundaries',agg_selection=True)
 ret = ops.execute()
 ## access the subsetted values. returned data is stored as a dictionary with the
 ## key the geometry identifier and the value an OcgCollection.
@@ -16,19 +26,29 @@ geoms = ret[1].variables['tasmax'].spatial.value
 ## time points
 times = ret[1].variables['tasmax'].temporal.value
 
-## aggregate the data by state boundary
-ops = OcgOperations(dataset=DATASET,geom='state_boundaries',aggregate=True,
+## return two variables ########################################################
+
+rdc = ocgis.RequestDatasetCollection([rd1,rd2])
+ops = ocgis.OcgOperations(rdc,snippet=True)
+ret = ops.execute()
+
+## aggregate the data by state boundary ########################################
+
+ops = ocgis.OcgOperations(dataset=rd1,geom='state_boundaries',aggregate=True,
                     snippet=True,spatial_operation='clip')
 ret = ops.execute()
 ## returned data has 51 keys. access the selection geometry for a particular 
 ## collection.
 ugeom = ret[1].ugeom
 
-## load a selection geometry from disk.
-sc = ShpCabinet()
+## load a selection geometry from disk #########################################
+
+sc = ocgis.ShpCabinet()
 geoms = sc.get_geoms('state_boundaries')
 ## load only a particular state
 geoms = sc.get_geoms('state_boundaries',attr_filter={'state_name':['California']})
 ## use california to subset
-ret = OcgOperations(dataset=DATASET,geom=geoms,spatial_operation='clip',
+ret = ocgis.OcgOperations(dataset=rd1,geom=geoms,spatial_operation='clip',
                     aggregate=True).execute()
+
+################################################################################

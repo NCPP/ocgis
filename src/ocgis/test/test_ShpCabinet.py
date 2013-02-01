4f682562d275bc30f9fee4f873da86c2422ad945
Ben Koziol <benkoziol@gmail.com>
2013-02-01 13:18:30 -0700

    minor - test changes.

--- src/ocgis/test/test_ShpCabinet.py
+++ src/ocgis/test/test_ShpCabinet.py
@@ -40,33 +40,38 @@ class Test(unittest.TestCase):
                 bounds = geom['geom'].bounds
                 self.assertTrue(bounds[0] >= pm)
                 
-    def test_none_for_attributes(self):
-        ## make temporary directory
-        path = get_temp_path(only_dir=True,nest=True)
-        try:
-            ## the shapefile key to use
-            key = 'state_boundaries'
-            ## get path to shapefile and copy directory to temporary location
-            sc = ShpCabinet()
-            origin = os.path.split(sc.get_shp_path(key))[0]
-            shutil.copytree(origin,os.path.join(path,key))
-            ## remove original config file
-            os.remove(os.path.join(path,key,key+'.cfg'))
-            ## write config file
-            config = ConfigParser.ConfigParser()
-            config.add_section('mapping')
-            config.set('mapping','ugid','none')
-            config.set('mapping','attributes','none')
-            with open(os.path.join(path,key,key+'.cfg'),'w') as f:
-                config.write(f)
-            ## load data
-            sc = ShpCabinet(path)
-            geoms = sc.get_geoms(key)
-            
-            for geom in geoms:
-                self.assertEqual(set(['ugid','geom']),set(geom.keys()))
-        finally:
-            shutil.rmtree(path)
+    def test_attribute_flags(self):
+        attr_flags = ['none','all']
+        for attr_flag in attr_flags:
+            ## make temporary directory
+            path = get_temp_path(only_dir=True,nest=True)
+            try:
+                ## the shapefile key to use
+                key = 'state_boundaries'
+                ## get path to shapefile and copy directory to temporary location
+                sc = ShpCabinet()
+                origin = os.path.split(sc.get_shp_path(key))[0]
+                shutil.copytree(origin,os.path.join(path,key))
+                ## remove original config file
+                os.remove(os.path.join(path,key,key+'.cfg'))
+                ## write config file
+                config = ConfigParser.ConfigParser()
+                config.add_section('mapping')
+                config.set('mapping','ugid','none')
+                config.set('mapping','attributes',attr_flag)
+                with open(os.path.join(path,key,key+'.cfg'),'w') as f:
+                    config.write(f)
+                ## load data
+                sc = ShpCabinet(path)
+                geoms = sc.get_geoms(key)
+                
+                for geom in geoms:
+                    if attr_flag == 'none':
+                        self.assertEqual(set(['ugid','geom']),set(geom.keys()))
+                    else:
+                        import ipdb;ipdb.set_trace()
+            finally:
+                shutil.rmtree(path)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']

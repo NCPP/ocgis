bb31effb3fba1f7f6a3abc36310e0e174137ba86
Ben Koziol <benkoziol@gmail.com>
2013-02-21 14:57:56 -0700

    found bug in SelectUgid.

--- examples/indices/demo.py
+++ examples/indices/demo.py
@@ -1,4 +1,5 @@
 import ocgis
+import os
 
 
 ## directory holding data files. this directory and it subdirectories will be
@@ -9,6 +10,8 @@ DIR_OUTPUT = '/home/local/WX/ben.koziol/Dropbox/nesii/project/ocg/presentation/2
 ## state identifiers in the state shapefile that together comprise the
 ## southwestern united states.
 SW_IDS = [25,23,49,37,42]
+## set to true until final execution
+SNIPPET = True
 
 
 ## update environmental variables.
@@ -16,3 +19,14 @@ ocgis.env.DIR_DATA = DIR_DATA
 ocgis.env.DIR_OUTPUT = DIR_OUTPUT
 ## pull data files together into a single collections of request datasets.
 rdc = ocgis.RequestDatasetCollection()
+for ii,filename in enumerate(os.listdir(ocgis.env.DIR_DATA)):
+    variable = filename.split('_')[0]
+    alias = variable + '_' + str(ii)
+    rd = ocgis.RequestDataset(filename,variable,alias=alias)
+    rdc.update(rd)
+## construct operations
+ops = ocgis.OcgOperations(rdc,
+                          snippet=SNIPPET,
+                          geom='state_boundaries',
+                          select_ugid=SW_IDS,
+                          output_format='csv')

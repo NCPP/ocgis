fd2014ddae68e56830c4f59f477deca61f82eb45
Ben Koziol <benkoziol@gmail.com>
2013-03-04 16:17:08 -0700

    fixed setup.py bug.

--- setup.py
+++ setup.py
@@ -51,14 +51,15 @@ def install(version='0.04.01b'):
                 ret.append(tail)
         ret.reverse()
         return('.'.join(ret))
-    package_dir = {'':'src'}
-    src_path = os.path.join(os.path.split(__file__)[0],package_dir.keys()[0],package_dir.values()[0],'ocgis')
+    relative_path = os.path.split(__file__)[0]
+    package_dir = {'':os.path.join(relative_path,'src')}
+    src_path = os.path.join(relative_path,package_dir.keys()[0],package_dir.values()[0],'ocgis')
     packages = []
     for dirpath,dirnames,filenames in os.walk(src_path):
         if '__init__.py' in filenames:
             package = _get_dot_(dirpath)
             packages.append(package)
-    import ipdb;ipdb.set_trace()
+
     ## run the installation
     setup(name='ocgis',
           version=version,

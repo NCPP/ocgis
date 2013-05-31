9aaea6894fa15e1e4931ed55699b68939ba8c587
Ben Koziol <benkoziol@gmail.com>
2013-05-31 12:54:34 -0600

    new logging implementation for subset

--- src/ocgis/api/subset.py
+++ src/ocgis/api/subset.py
@@ -8,7 +8,15 @@ from ocgis.exc import EmptyData, ExtentError, MaskedDataError
 from ocgis.interface.projection import WGS84
 from ocgis.util.spatial.wrap import Wrapper
 from copy import deepcopy
+import logging
+from ocgis.util.logging_ocgis import ocgis_lh
 
+
+## create local logger if necessary
+if env.VERBOSE or env.ENABLE_FILE_LOGGING:
+    subset_log = logging.getLogger('subset')
+else:
+    subset_log = None
 
 class SubsetOperation(object):
     
@@ -18,14 +26,14 @@ class SubsetOperation(object):
         self.nprocs = nprocs
         
         if validate:
-            if env.VERBOSE: print('validating request datasets...')
+            ocgis_lh('validating request datasets',subset_log)
             ops.dataset.validate()
 
         ## create the calculation engine
         if self.ops.calc is None:
             self.cengine = None
         else:
-            if env.VERBOSE: print('initializing calculation engine...')
+            ocgis_lh('initializing calculation engine',subset_log)
             self.cengine = OcgCalculationEngine(self.ops.calc_grouping,
                                            self.ops.calc,
                                            raw=self.ops.calc_raw,
@@ -35,7 +43,7 @@ class SubsetOperation(object):
         ## on, the time range should be set in the operations dictionary.
         if self.ops.snippet is True:
             ##TODO: move snippet to iteration
-            if env.VERBOSE: print('getting snippet bounds...')
+            ocgis_lh('getting snippet bounds',subset_log)
             for rd in self.ops.dataset:
                 rd.level_range = [1,1]
                 ods = rd.ds
@@ -80,19 +88,20 @@ class SubsetOperation(object):
         ''':rtype: tuple'''
         ## if there is no geometry, yield None.
         if self.ops.geom is None:
-            yield(self,None)
+            ocgis_lh('returning entire spatial domain - no selection geometry',subset_log)
+            yield(self,None,subset_log)
         ## iterator through geometries in the ShpDataset
         elif isinstance(self.ops.geom,ShpDataset):
-            if env.VERBOSE: print('{0} geometry(s) to process.'.format(len(self.ops.geom)))
+            ocgis_lh('{0} geometry(s) to process'.format(len(self.ops.geom)),subset_log)
             for geom in self.ops.geom:
-                yield(self,geom)
+                yield(self,geom,subset_log)
         ## otherwise, the data is likely a GeometryDataset with a single value.
         ## just return it.
         else:
-            if env.VERBOSE: print('1 geometry to process.')
-            yield(self,self.ops.geom)
+            ocgis_lh('1 geometry to process'.format(len(self.ops.geom)),subset_log)
+            yield(self,self.ops.geom,subset_log)
             
-def get_collection((so,geom)):
+def get_collection((so,geom,logger)):
     '''
     :type so: SubsetOperation
     :type geom: None, GeometryDataset, ShpDataset
@@ -102,14 +111,13 @@ def get_collection((so,geom)):
     ## initialize the collection object to store the subsetted data.
     coll = RawCollection(ugeom=geom)
     ## perform the operations on each request dataset
-    if env.VERBOSE: print('{0} request dataset(s) to process.'.format(len(so.ops.dataset)))
+    ocgis_lh('{0} request dataset(s) to process'.format(len(so.ops.dataset)),logger)
+    ## reference the geometry ugid
+    ugid = None if geom is None else geom.spatial.uid[0]
     for request_dataset in so.ops.dataset:
-        if env.VERBOSE:
-            if geom is None:
-                msg = 'processing: alias={0}'.format(request_dataset.alias)
-            else:
-                msg = 'processing: ugid={0}, alias={1}'.format(geom.spatial.uid[0],request_dataset.alias)
-            print(msg)
+        ## reference the request dataset alias
+        alias = request_dataset.alias
+        ocgis_lh('processing',logger,level=logging.DEBUG,alias=alias,ugid=ugid)
         ## copy the geometry
         copy_geom = deepcopy(geom)
         ## reference the dataset object
@@ -128,9 +136,17 @@ def get_collection((so,geom)):
                 ## check projections adjusting projection the selection geometry
                 ## if necessary
                 if type(ods.spatial.projection) != type(copy_geom.spatial.projection):
+                    msg = 'projecting selection geometry to match input projection: {0} to {1}'
+                    msg = msg.format(copy_geom.spatial.projection.__class__.__name__,
+                                     ods.spatial.projection.__class__.__name__)
+                    ocgis_lh(msg,logger,alias=alias,ugid=ugid)
                     copy_geom.project(ods.spatial.projection)
+                else:
+                    ocgis_lh('projections match',logger,alias=alias,ugid=ugid)
                 ## unwrap the data if it is geographic and 360
                 if type(ods.spatial.projection) == WGS84 and ods.spatial.is_360:
+                    ocgis_lh('unwrapping selection geometry with axis={0}'.format(ods.spatial.pm),
+                             logger,alias=alias,ugid=ugid)
                     w = Wrapper(axis=ods.spatial.pm)
                     copy_geom.spatial.geom[0] = w.unwrap(deepcopy(copy_geom.spatial.geom[0]))
                 igeom = copy_geom.spatial.geom[0]
@@ -142,6 +158,8 @@ def get_collection((so,geom)):
                                      level=request_dataset.level_range)
                 ## aggregate the geometries and data if requested
                 if so.ops.aggregate:
+                    ocgis_lh('aggregating target geometries and area-weighting values',
+                             logger,alias=alias,ugid=ugid)
                     ## the new geometry will have the same id as the passed
                     ## geometry. if it does not have one, simple give it a value
                     ## of 1 as it is the only geometry requested for subsetting.
@@ -160,31 +178,35 @@ def get_collection((so,geom)):
                        ods.spatial.is_360 and \
                        so.ops.output_format != 'nc' and \
                        so.ops.vector_wrap:
+                        ocgis_lh('wrapping output geometries',logger,alias=alias,
+                                 ugid=ugid)
                         ods.spatial.vector.wrap()
                 ## check for all masked values
                 if not so.ops.file_only and ods.value.mask.all():
                     if so.ops.snippet or so.ops.allow_empty:
-                        if env.VERBOSE:
-                            if so.ops.snippet:
-                                print('all masked data encountered but allowed for snippet.')
-                            if so.ops.allow_empty:
-                                print('all masked data encountered but empty returns are allowed.')
+                        if so.ops.snippet:
+                            ocgis_lh('all masked data encountered but allowed for snippet',
+                                     logger,alias=alias,ugid=ugid,level=logging.WARN)
+                        if so.ops.allow_empty:
+                            ocgis_lh('all masked data encountered but empty returns allowed',
+                                     logger,alias=alias,ugid=ugid,level=logging.WARN)
                         pass
                     else:
-                        raise(MaskedDataError)
+                        ocgis_lh(None,logger,exc=MaskedDataError(),alias=alias,ugid=ugid)
             ## there may be no data returned - this may be real or could be an
             ## error. by default, empty returns are not allowed
             except EmptyData:
                 if so.ops.allow_empty:
-                    if env.VERBOSE:
-                        print('the geometric operations returned empty but empty returns are allowed')
+                    ocgis_lh('the geometric operations returned empty but empty returns are allowed',
+                             logger,alias=alias,ugid=ugid)
                     continue
                 else:
-                    raise(ExtentError(request_dataset))
+                    ocgis_lh(ExtentError(request_dataset),logger,alias=alias,ugid=ugid)
         coll.variables.update({request_dataset.alias:ods})
 
     ## if there are calculations, do those now and return a new type of collection
     if so.cengine is not None:
+        ocgis_lh('performing computations',logger,alias=alias,ugid=ugid)
         coll = so.cengine.execute(coll,file_only=so.ops.file_only)
     
     ## conversion of groups.

63a509a13bce416d292022c171fadf91f6376458
Ben Koziol <benkoziol@gmail.com>
2013-06-25 19:11:23 -0600

    added format_return to ocgis import

--- src/ocgis/__init__.py
+++ src/ocgis/__init__.py
@@ -8,6 +8,7 @@ from util.inspect import Inspect
 from api.request import RequestDataset, RequestDatasetCollection
 from interface.geometry import GeometryDataset
 from interface.shp import ShpDataset
+from util.zipper import format_return
 
 from osgeo import ogr
 ## tell ogr to raise exceptions

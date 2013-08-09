cf30add3f9b78255c5b7258bc72acea1ed67b7df
Ben Koziol <benkoziol@gmail.com>
2013-08-09 09:26:13 -0600

    modifications to frequency duration test

--- src/ocgis/test/test_calc.py
+++ src/ocgis/test/test_calc.py
@@ -91,13 +91,15 @@ class Test(TestBase):
         variable = 'tasmax'
         ocgis.env.DIR_DATA = '/usr/local/climate_data'
         
-        for output_format in ['csv','csv+','shp']:
-            ops = OcgOperations(dataset={'uri':uri,'variable':variable,'time_range':[datetime.datetime(1971, 1, 1, 0, 0), datetime.datetime(1972, 12, 31, 0, 0)]},
+        for output_format in ['csv+','shp','csv']:
+            ops = OcgOperations(dataset={'uri':uri,
+                                         'variable':variable,
+                                         'time_region':{'year':None,'month':[7]}},
                                 output_format=output_format,prefix=output_format,
-                                calc=[{'name': 'Frequency Duration', 'func': 'freq_duration', 'kwds': {'threshold': 20.0, 'operation': 'gte'}}],
+                                calc=[{'name': 'Frequency Duration', 'func': 'freq_duration', 'kwds': {'threshold': 25.0, 'operation': 'gte'}}],
                                 calc_grouping=['month','year'],
-                                geom='qed_city_centroids',select_ugid=[1,2,3],aggregate=True,
-                                calc_raw=False,
+                                geom='us_counties',select_ugid=[2778],aggregate=True,
+                                calc_raw=False,spatial_operation='clip',
                                 headers=['did', 'ugid', 'gid', 'year', 'month', 'day', 'variable', 'calc_name', 'value'],)
             ret = ops.execute()
     

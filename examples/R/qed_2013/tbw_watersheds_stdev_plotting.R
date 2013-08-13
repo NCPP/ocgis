8fc9f12b94bd947c183ac75471e607ca494c5c42
Ben Koziol <benkoziol@gmail.com>
2013-08-13 14:24:41 -0600

    changes to watershed plotting

--- examples/R/qed_2013/tbw_watersheds_stdev_plotting.R
+++ examples/R/qed_2013/tbw_watersheds_stdev_plotting.R
@@ -15,6 +15,11 @@ data$TIME = as.Date(data$TIME)
 data.nomask = subset(data,data$VALUE != 1e20)
 ## join the output data file with the geometry descriptions
 data.joined = merge(data.nomask,data.geom,by="UGID",all=TRUE)
+## remove extra GIDs value data to account for cells overlapping watersheds
+data.unique = data.nomask
+data.unique$UGID = NULL
+data.unique = unique(data.unique)
+p90 = quantile(data.unique$VALUE,0.9)
 
 data.joined = subset(data.joined,WATERSHED %in% c('ALAFIA RIVER','COASTAL CREEKS','BULLFROG CREEK'))
 
@@ -30,14 +35,20 @@ wstats = function(df){
   ret['max'] = max(VALUE)
   ret['n'] = length(VALUE)
   ret['sum'] = sum(VALUE)
+  ret['p90'] = quantile(VALUE,0.9)
   detach(df)
   return(as.data.frame(ret))
 }
 
 data.stats = ddply(data.joined,.(WATERSHED,TIME,VARIABLE,ALIAS),wstats)
+data.stats$Legend = data.stats$ALIAS
 
 p = ggplot(data=data.stats,aes(x=TIME,y=sd))
-p = p + geom_line(aes(color=ALIAS,linetype=ALIAS),size=0.75)
+p = p + geom_line(aes(color=Legend,linetype=Legend),size=0.75)
 p = p + labs(title='Standard Deviation by Watershed (mm/day)',x='Time',y='Precipitation (mm/day)')
 p = p + facet_grid(WATERSHED~.)
+p = p + theme(legend.text=element_text(size=16),axis.text.x=element_text(size=16),axis.text.y=element_text(size=16))
+p = p + theme(axis.title.x=element_text(size=18),axis.title.y=element_text(size=18))
+p = p + theme(plot.title=element_text(size=19),strip.text.y=element_text(size=16))
+p = p + theme(legend.title=element_text(size=16))
 print(p)

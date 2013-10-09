413e6c814228111122b0edf0f799bbce4506dc37
Ben Koziol <benkoziol@gmail.com>
2013-10-09 08:46:30 -0600

    added example netCDF4-python script to split a file with a projection axis into multiple files.

---
+++ examples/remove_projection_axis.py
@@ -0,0 +1,54 @@
+import netCDF4 as nc
+import os
+
+
+## path to input file
+NCFILE = '/home/local/WX/ben.koziol/climate_data/projection-axis/Canada_Projections.nc'
+## directory to write projection files to
+OUTDIR = '/tmp/foo'
+## string template for making unique output names
+OUT_TEMPLATE = 'Canada_Projections_p{0}.nc'
+
+
+def write_projection_file(ds,pidx):
+    print('writing projection: {0}'.format(pidx+1))
+    ## path to the nc file holding a single projection
+    out_path = os.path.join(OUTDIR,OUT_TEMPLATE.format(str(pidx+1).zfill(2)))
+    ## open the output dataset with the same format as the input
+    ds_out = nc.Dataset(out_path,'w',format=ds.file_format)
+    try:
+        ## transfer attributes to new datasets
+        ds_out.setncatts(ds.__dict__)
+        ## construct output dimensions - note the projection dimension is missing
+        for dimname in ['time','latitude','longitude']:
+            ds_out.createDimension(dimname,size=len(ds.dimensions[dimname]))
+        ## create the coordinate variables
+        for varname in ['time','latitude','longitude']:
+            r_dvar = ds.variables[varname]
+            var = ds_out.createVariable(varname,r_dvar.dtype,r_dvar.dimensions)
+            var.setncatts(r_dvar.__dict__)
+            ## this is where the data is pulled and assigned to the new variable.
+            ## the sync at the end is not really necessary for smaller data blocks
+            ## but may be useful for larger files
+            var[:] = r_dvar[:]
+            ds_out.sync()
+        ## create and fill the data variable using the projection index
+        orig_var = ds.variables['Tavg']
+        data_var = ds_out.createVariable('Tavg',orig_var.dtype,('time','latitude','longitude'))
+        data_var.setncatts(orig_var.__dict__)
+        data_var[:] = orig_var[pidx,:,:,:]
+    finally:
+        ds_out.close()
+
+def main():
+    ds = nc.Dataset(NCFILE,'r')
+    try:
+        for pidx in range(len(ds.dimensions['projection'])):
+            write_projection_file(ds,pidx)
+    finally:
+        ds.close()
+    print('success.')
+
+
+if __name__ == '__main__':
+    main()

e77ec2fc6880aab33ab7116bf1b9c4bf991226d3
Ben Koziol <benkoziol@gmail.com>
2014-05-20 07:53:41 -0600

    added time series example

---
+++ examples/time_series.py
@@ -0,0 +1,14 @@
+from tempfile import mkdtemp
+import ocgis
+
+
+ocgis.env.DIR_OUTPUT = mkdtemp()
+uri = '/usr/local/climate_data/CanCM4/tas_day_CanCM4_decadal2011_r2i1p1_20120101-20211231.nc'
+variable = 'tas'
+
+
+rd = ocgis.RequestDataset(uri=uri, variable=variable)
+ops = ocgis.OcgOperations(dataset=rd, geom=[-73.8, 42.7])
+ret = ops.execute()
+# print(ret)
+field = ret[1]['tas']

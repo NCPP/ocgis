c62b353728667a583bf6444caace0f329a7222b2
Ben Koziol <benkoziol@gmail.com>
2014-06-03 16:39:21 -0600

    doc changes

--- doc/sphinx_examples/install_dependencies_ubuntu.sh
+++ doc/sphinx_examples/install_dependencies_ubuntu.sh
@@ -8,11 +8,16 @@ sudo pip install numpy netCDF4 shapely fiona rtree
 ## osgeo ##
 ###########
 
+## If this fails, try the apt-get install below...
+
 ## http://stackoverflow.com/questions/11336153/python-gdal-package-missing-header-file-when-installing-via-pip
 pip install --no-install GDAL
 cd /tmp/pip_build_ubuntu/GDAL
 python setup.py build_ext --include-dirs=/usr/include/gdal
 sudo python setup.py install
+
+## If the above install fails, you can install from apt-get...
+#sudo apt-get install python-gdal
 
 #############
 ## cfunits ##
@@ -32,6 +37,7 @@ cd cfunits-$CFUNITS_VER
 sudo python setup.py install
 ## installation does not copy UDUNITS database
 CFUNITS_SETUP_DIR=`pwd`
-cd
-python -c 'import cfunits,os,subprocess;cfunits_path=os.path.split(cfunits.__file__)[0];subprocess.call(["export","CFUNITS_INSTALL_DIR={0}".format(cfunits_path)],shell=True)'
+## assumes a standard location. the installation directory may be retrieved by running the command:
+##  python -c "import cfunits, os;print(os.path.split(cfunits.__file__)[0])"
+CFUNITS_INSTALL_DIR=/usr/local/lib/python2.7/dist-packages/cfunits
 sudo cp -r $CFUNITS_SETUP_DIR/cfunits/etc $CFUNITS_INSTALL_DIR

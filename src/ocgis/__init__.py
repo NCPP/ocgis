26c6d43471fe79f3527f57921e2881e69b9b4882
Ben Koziol <benkoziol@gmail.com>
2014-12-26 10:04:42 -0700

    added imports to ocgis.__init__

--- src/ocgis/__init__.py
+++ src/ocgis/__init__.py
@@ -1,19 +1,22 @@
 from osgeo import ogr, osr
 
 from ocgis.util.environment import env
+
 from ocgis.api.collection import SpatialCollection
 from ocgis.api.operations import OcgOperations
 from ocgis.api.request.base import RequestDataset, RequestDatasetCollection
 from ocgis.calc.library.register import FunctionRegistry
 from ocgis.interface.base import crs
 from ocgis.interface.base.crs import CoordinateReferenceSystem
-from ocgis.interface.base.dimension.spatial import SpatialDimension
+from ocgis.interface.base.dimension.spatial import SpatialDimension, SpatialGridDimension, SpatialGeometryDimension, \
+    SpatialGeometryPolygonDimension, SpatialGeometryPointDimension
 from ocgis.interface.base.field import Field
 from ocgis.util.inspect import Inspect
 from ocgis.util.shp_cabinet import ShpCabinet, ShpCabinetIterator
 from ocgis.util.zipper import format_return
 from ocgis.interface.base.dimension.temporal import TemporalDimension
 from ocgis.interface.base.variable import Variable
+from ocgis.interface.base.dimension.base import VectorDimension
 
 
 __version__ = '1.0.1'

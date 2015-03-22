3c7308866bb6a2c733e4b4ac9b830651d0a9447e
Ben Koziol <benkoziol@gmail.com>
2015-03-22 10:40:04 -0600

    made simple tests the main target for the run test function

--- src/ocgis/test/run_tests.py
+++ src/ocgis/test/run_tests.py
@@ -4,12 +4,17 @@ import sys
 
 
 def run(exclude='!slow,!remote,!esmpy7', dir_shpcabinet='~/data/ocgis_test_data/shp',
-        dir_test_data='~/data/ocgis_test_data'):
-    from ocgis import test
+        dir_test_data='~/data/ocgis_test_data', simple=True):
+    if simple:
+        from ocgis.test import test_simple
+        target = test_simple
+    else:
+        from ocgis import test
+        target = test
 
-    module_name = os.path.split(test.__file__)[0]
+    path = os.path.split(target.__file__)[0]
     os.environ['OCGIS_DIR_SHPCABINET'] = os.path.expanduser(dir_shpcabinet)
     os.environ['OCGIS_DIR_TEST_DATA'] = os.path.expanduser(dir_test_data)
-    result = nose.run(argv=[sys.argv[0], module_name, '-a', exclude])
+    result = nose.run(argv=[sys.argv[0], path, '-a', exclude])
     if not result:
         sys.exit(1)

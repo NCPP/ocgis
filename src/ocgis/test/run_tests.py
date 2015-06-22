0c627aa5e73f43f86c408a87717523c7e8d273f5
bekozi <benkoziol@gmail.com>
2015-06-22 15:43:32 -0600

    changed test targets to use nose plugins

--- src/ocgis/test/run_tests.py
+++ src/ocgis/test/run_tests.py
@@ -7,9 +7,38 @@ import nose
 import ocgis
 
 
-class RunNoESMF(nose.plugins.Plugin):
+class RunOcgis(nose.plugins.Plugin):
+    name = 'ocgis'
+    target = 'ocgis'
+    _attrs_standard = '!versions,!esmpy7,!slow,!remote'
+
+    def __init__(self, *args, **kwargs):
+        self.attrs = kwargs.pop('attrs', self._attrs_standard)
+        self.verbose = kwargs.pop('verbose', True)
+
+        dir_shpcabinet = kwargs.pop('dir_shpcabinet', '~/data/ocgis_test_data/shp')
+        dir_test_data = kwargs.pop('dir_test_data', '~/data/ocgis_test_data')
+        os.environ['OCGIS_DIR_SHPCABINET'] = os.path.expanduser(dir_shpcabinet)
+        os.environ['OCGIS_DIR_TEST_DATA'] = os.path.expanduser(dir_test_data)
+
+        super(RunOcgis, self).__init__(*args, **kwargs)
+
+    def get_argv(self):
+        argv = [sys.argv[0], '--with-{0}'.format(self.name), '-s', '-a', self.attrs]
+        if self.verbose:
+            argv.append('-v')
+        argv += self._get_argv_adds_()
+        argv.append(self.target)
+        return argv
+
+    def _get_argv_adds_(self):
+        return []
+
+
+class RunNoESMF(RunOcgis):
     name = 'no-esmf'
     _exclude = 'test_regrid.test_base|test_conv.test_esmpy'
+    _attrs_standard = '!esmf,!esmpy7,!remote,!slow'
 
     def wantFile(self, file):
         match = re.search(self._exclude, file)
@@ -20,38 +49,33 @@ class RunNoESMF(nose.plugins.Plugin):
         return ret
 
 
-def run(attrs='simple', dir_shpcabinet='~/data/ocgis_test_data/shp', dir_test_data='~/data/ocgis_test_data',
-        verbose=False):
+class RunSimple(RunOcgis):
+    _attrs_standard = 'simple,!optional'
 
-    os.environ['OCGIS_DIR_SHPCABINET'] = os.path.expanduser(dir_shpcabinet)
-    os.environ['OCGIS_DIR_TEST_DATA'] = os.path.expanduser(dir_test_data)
-    argv = [sys.argv[0], 'ocgis']
-    if attrs is not None:
-        argv += ['-a', attrs]
-    if verbose:
-        argv.append('-v')
-    result = nose.run(argv=argv)
+    @property
+    def target(self):
+        path = os.path.realpath(ocgis.__file__)
+        path = os.path.split(path)[0]
+        path = os.path.join(path, 'test', 'test_simple')
+        return path
+
+
+def _run_tests_(nose_plugin):
+    result = nose.run(addplugins=[nose_plugin], argv=nose_plugin.get_argv())
     if not result:
         sys.exit(1)
 
 
-def run_no_esmf(dir_shpcabinet='~/data/ocgis_test_data/shp', dir_test_data='~/data/ocgis_test_data',
-                attrs='!esmf,!esmpy7,!remote,!slow'):
-    os.environ['OCGIS_DIR_SHPCABINET'] = os.path.expanduser(dir_shpcabinet)
-    os.environ['OCGIS_DIR_TEST_DATA'] = os.path.expanduser(dir_test_data)
-    result = nose.run(addplugins=[RunNoESMF()],
-                      argv=[__file__, '-vs', '-a', attrs, '--with-no-esmf', 'ocgis'])
-    if not result:
-        sys.exit(1)
+def run_all(**kwargs):
+    nose_plugin = RunOcgis(**kwargs)
+    _run_tests_(nose_plugin)
 
 
-def run_simple(verbose=False):
-    path = os.path.realpath(ocgis.__file__)
-    path = os.path.split(path)[0]
-    path = os.path.join(path, 'test', 'test_simple')
-    argv = [sys.argv[0], '-a', '!optional', path]
-    if verbose:
-        argv.append('-v')
-    result = nose.run(argv=argv)
-    if not result:
-        sys.exit(1)
+def run_no_esmf(**kwargs):
+    nose_plugin = RunNoESMF(**kwargs)
+    _run_tests_(nose_plugin)
+
+
+def run_simple(**kwargs):
+    nose_plugin = RunSimple(**kwargs)
+    _run_tests_(nose_plugin)

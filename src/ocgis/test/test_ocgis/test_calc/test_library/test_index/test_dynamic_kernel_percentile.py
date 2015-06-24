fixed slow tests for precision and api changes

src/ocgis/test/test_ocgis/test_calc/test_library/test_index/test_dynamic_kernel_percentile.py
@@ -1,7 +1,7 @@
-import netCDF4 as nc
 import itertools
 import datetime
 
+import netCDF4 as nc
 import numpy as np
 
 from ocgis.api.operations import OcgOperations
@@ -11,153 +11,127 @@ from ocgis.calc.library.index.dynamic_kernel_percentile import DynamicDailyKerne
 
 
 class TestDynamicDailyKernelPercentileThreshold(TestBase):
-    
     def get_percentile_reference(self):
-        years = [2001,2002,2003]
-        days = [3,4,5,6,7]
-        
+        years = [2001, 2002, 2003]
+        days = [3, 4, 5, 6, 7]
+
         dates = []
-        for year,day in itertools.product(years,days):
-            dates.append(datetime.datetime(year,6,day,12))
-            
+        for year, day in itertools.product(years, days):
+            dates.append(datetime.datetime(year, 6, day, 12))
+
         ds = nc.Dataset(self.test_data.get_uri('cancm4_tas'))
         try:
             calendar = ds.variables['time'].calendar
             units = ds.variables['time'].units
-            ncdates = nc.num2date(ds.variables['time'][:],units,calendar=calendar)
+            ncdates = nc.num2date(ds.variables['time'][:], units, calendar=calendar)
             indices = []
-            for ii,ndate in enumerate(ncdates):
+            for ii, ndate in enumerate(ncdates):
                 if ndate in dates:
                     indices.append(ii)
-            tas = ds.variables['tas'][indices,:,:]
-            ret = np.percentile(tas,10,axis=0)
+            tas = ds.variables['tas'][indices, :, :]
+            ret = np.percentile(tas, 10, axis=0)
         finally:
             ds.close()
-            
-        return(ret)
-    
+
+        return ret
+
     def test_constructor(self):
         DynamicDailyKernelPercentileThreshold()
-        
-    # def test_get_calendar_day_window_(self):
-    #     cday_index = np.arange(0,365)
-    #     rr = DynamicDailyKernelPercentileThreshold._get_calendar_day_window_
-    #     width = 5
-    #
-    #     target_cday_index = 0
-    #     ret = rr(cday_index,target_cday_index,width)
-    #     self.assertNumpyAll(ret,np.array([0,363,364,1,2]))
-    #
-    #     target_cday_index = 15
-    #     ret = rr(cday_index,target_cday_index,width)
-    #     self.assertNumpyAll(ret,np.array([15,13,14,16,17]))
-    #
-    #     target_cday_index = 363
-    #     ret = rr(cday_index,target_cday_index,width)
-    #     self.assertNumpyAll(ret,np.array([363,361,362,0,364]))
-    
+
     def test_calculate(self):
-        ## daily data for three years is wanted for the test. subset a CMIP5
-        ## decadal simulation to use for input into the computation.
+        # daily data for three years is wanted for the test. subset a CMIP5 decadal simulation to use for input into the 
+        # computation.
         rd = self.test_data.get_rd('cancm4_tas')
         field = rd.get()
-        field = field.get_between('temporal',datetime.datetime(2001,1,1),datetime.datetime(2003,12,31,23,59))
-        ## the calculation will be for months and years. set the temporal grouping.
-        temporal_group = field.temporal.get_grouping(['month','year'])
-        ## create calculation object
+        field = field.get_between('temporal', datetime.datetime(2001, 1, 1), datetime.datetime(2003, 12, 31, 23, 59))
+        # the calculation will be for months and years. set the temporal grouping.
+        temporal_group = field.temporal.get_grouping(['month', 'year'])
+        # create calculation object
         percentile = 10
         width = 5
         operation = 'lt'
-        kwds = dict(percentile=percentile,width=width,operation=operation)
+        kwds = dict(percentile=percentile, width=width, operation=operation)
         value = field.variables['tas'].value
         dkp = DynamicDailyKernelPercentileThreshold(tgd=temporal_group,
-                                                    parms=kwds,field=field,alias='tg10p')        
-        dperc = dkp.get_daily_percentile(value,field.temporal.value_datetime,percentile,width)
-        # select = np.logical_and(dperc['month'] == 6,dperc['day'] == 5)
-        # to_test = dperc[select]['percentile'][0]
+                                                    parms=kwds, field=field, alias='tg10p')
+        dperc = dkp.get_daily_percentile(value, field.temporal.value_datetime, percentile, width)
         to_test = dperc[6, 5]
         ref = self.get_percentile_reference()
-        self.assertNumpyAll(to_test,ref)
-        
+        self.assertNumpyAll(to_test, ref)
+
         ret = dkp.execute()
-        self.assertEqual(ret['tg10p'].value.shape,(1,36,1,64,128))
-        self.assertAlmostEqual(ret['tg10p'].value.mean(),3.6267225477430554)
-    
+        self.assertEqual(ret['tg10p'].value.shape, (1, 36, 1, 64, 128))
+        self.assertAlmostEqual(ret['tg10p'].value.mean(), 3.6267225477430554)
+
     @attr('slow')
     def test_operations(self):
         uri = self.test_data.get_uri('cancm4_tas')
-        rd = RequestDataset(uri=uri,
-                            variable='tas',
-#                            time_range=[datetime.datetime(2001,1,1),datetime.datetime(2003,12,31,23,59)]
-                            )
-        calc_grouping = ['month','year']
-        calc = [{'func':'dynamic_kernel_percentile_threshold','name':'tg10p','kwds':{'percentile':10,'width':5,'operation':'lt'}}]
-        ops = OcgOperations(dataset=rd,calc_grouping=calc_grouping,calc=calc,
-                            output_format='nc')
+        rd = RequestDataset(uri=uri, variable='tas')
+        calc_grouping = ['month', 'year']
+        calc = [{'func': 'dynamic_kernel_percentile_threshold', 'name': 'tg10p',
+                 'kwds': {'percentile': 10, 'width': 5, 'operation': 'lt'}}]
+        ops = OcgOperations(dataset=rd, calc_grouping=calc_grouping, calc=calc, output_format='nc')
         ret = ops.execute()
-        
+
         with nc_scope(ret) as ds:
             ref = ds.variables['tg10p'][:]
-            self.assertAlmostEqual(ref.mean(),2.9778004964192708)
-    
+            self.assertAlmostEqual(ref.mean(), 2.9778006)
+
     @attr('slow')
     def test_operations_two_steps(self):
-        ## get the request dataset to use as the basis for the percentiles
+        # get the request dataset to use as the basis for the percentiles
         uri = self.test_data.get_uri('cancm4_tas')
         variable = 'tas'
-        rd = RequestDataset(uri=uri,variable=variable)
-        ## this is the underly OCGIS dataset object
+        rd = RequestDataset(uri=uri, variable=variable)
+        # this is the underly OCGIS dataset object
         nc_basis = rd.get()
-        
-        ## NOTE: if you want to subset the basis by time, this step is necessary
-#        nc_basis = nc_basis.get_between('temporal',datetime.datetime(2001,1,1),datetime.datetime(2003,12,31,23,59))
-        
-        ## these are the values to use when calculating the percentile basis. it
-        ## may be good to wrap this in a function to have memory freed after the
-        ## percentile structure array is computed.
+
+        # NOTE: if you want to subset the basis by time, this step is necessary
+        # nc_basis = nc_basis.get_between('temporal',datetime.datetime(2001,1,1),datetime.datetime(2003,12,31,23,59))
+
+        # these are the values to use when calculating the percentile basis. it may be good to wrap this in a function
+        # to have memory freed after the percentile structure array is computed.
         all_values = nc_basis.variables[variable].value
-        ## these are the datetime objects used for window creation
+        # these are the datetime objects used for window creation
         temporal = nc_basis.temporal.value_datetime
-        ## additional parameters for calculating the basis
+        # additional parameters for calculating the basis
         percentile = 10
         width = 5
-        ## get the structure array
+        # get the structure array
         from ocgis.calc.library.index.dynamic_kernel_percentile import DynamicDailyKernelPercentileThreshold
-        daily_percentile = DynamicDailyKernelPercentileThreshold.get_daily_percentile(all_values,temporal,percentile,width)
-        
-        ## perform the calculation using the precomputed basis. in this case,
-        ## the basis and target datasets are the same, so the RequestDataset is
-        ## reused.
-        calc_grouping = ['month','year']
-        kwds = {'percentile':percentile,'width':width,'operation':'lt','daily_percentile':daily_percentile}
-        calc = [{'func':'dynamic_kernel_percentile_threshold','name':'tg10p','kwds':kwds}]
-        ops = OcgOperations(dataset=rd,calc_grouping=calc_grouping,calc=calc,
+
+        daily_percentile = DynamicDailyKernelPercentileThreshold.get_daily_percentile(all_values, temporal, percentile,
+                                                                                      width)
+
+        # perform the calculation using the precomputed basis. in this case, the basis and target datasets are the same,
+        # so the RequestDataset is reused.
+        calc_grouping = ['month', 'year']
+        kwds = {'percentile': percentile, 'width': width, 'operation': 'lt', 'daily_percentile': daily_percentile}
+        calc = [{'func': 'dynamic_kernel_percentile_threshold', 'name': 'tg10p', 'kwds': kwds}]
+        ops = OcgOperations(dataset=rd, calc_grouping=calc_grouping, calc=calc,
                             output_format='nc')
         ret = ops.execute()
-        
-        ## if we want to return the values as a three-dimenional numpy array the
-        ## method below will do this. note the interface arrangement for the next
-        ## release will alter this slightly.
-        ops = OcgOperations(dataset=rd,calc_grouping=calc_grouping,calc=calc,
+
+        # if we want to return the values as a three-dimenional numpy array the method below will do this. note the
+        # interface arrangement for the next release will alter this slightly.
+        ops = OcgOperations(dataset=rd, calc_grouping=calc_grouping, calc=calc,
                             output_format='numpy')
         arrs = ops.execute()
-        ## reference the returned numpy data. the first key is the geometry identifier.
-        ## 1 in this case as this is the default for no selection geometry. the second
-        ## key is the request dataset alias and the third is the calculation name.
-        ## the variable name is appended to the end of the calculation to maintain
-        ## a unique identifier.
+        # reference the returned numpy data. the first key is the geometry identifier. 1 in this case as this is the
+        # default for no selection geometry. the second key is the request dataset alias and the third is the
+        # calculation name. the variable name is appended to the end of the calculation to maintain a unique identifier.
         tg10p = arrs[1]['tas'].variables['tg10p'].value
-        ## if we want the date information for the temporal groups date attributes
+        # if we want the date information for the temporal groups date attributes
         date_parts = arrs[1]['tas'].temporal.date_parts
-        assert(date_parts.shape[0] == tg10p.shape[1])
-        ## these are the representative datetime objects
-        rep_dt = arrs[1]['tas'].temporal.value_datetime
-        ## and these are the lower and upper time bounds on the date groups
-        bin_bounds = arrs[1]['tas'].temporal.bounds_datetime
-        
-        ## confirm we have values for each month and year (12*10)
+        assert (date_parts.shape[0] == tg10p.shape[1])
+        # these are the representative datetime objects
+        # rep_dt = arrs[1]['tas'].temporal.value_datetime
+        # and these are the lower and upper time bounds on the date groups
+        # bin_bounds = arrs[1]['tas'].temporal.bounds_datetime
+
+        # confirm we have values for each month and year (12*10)
         ret_ds = nc.Dataset(ret)
         try:
-            self.assertEqual(ret_ds.variables['tg10p'].shape,(120,64,128))
+            self.assertEqual(ret_ds.variables['tg10p'].shape, (120, 64, 128))
         finally:
             ret_ds.close()

src/ocgis/test/test_real_data/test_narccap.py
@@ -1,5 +1,5 @@
-import unittest
 import os
+
 import numpy as np
 
 from ocgis.test.base import TestBase, nc_scope, attr
@@ -11,70 +11,74 @@ from ocgis.interface.base.crs import CFRotatedPole, CFWGS84
 
 
 class TestRotatedPole(TestBase):
-    
     def test_validation(self):
-        ## CFRotatedPole is not an appropriate output crs. it may also not be
-        ## transformed to anything but WGS84
+        # CFRotatedPole is not an appropriate output crs. it may also not be transformed to anything but WGS84
         rd = self.test_data.get_rd('narccap_rotated_pole')
         with self.assertRaises(DefinitionValidationError):
-            OcgOperations(dataset=rd,output_crs=CFRotatedPole(grid_north_pole_latitude=5,
-                                                              grid_north_pole_longitude=5))
-        ## this is an okay output coordinate system for the two input coordinate
-        ## systems
+            OcgOperations(dataset=rd, output_crs=CFRotatedPole(grid_north_pole_latitude=5,
+                                                               grid_north_pole_longitude=5))
+        # this is an okay output coordinate system for the two input coordinate systems
         rd2 = self.test_data.get_rd('narccap_lambert_conformal')
-        OcgOperations(dataset=[rd,rd2],output_crs=CFWGS84())
-        
+        OcgOperations(dataset=[rd, rd2], output_crs=CFWGS84())
+
     def test_calculation(self):
-        rd = self.test_data.get_rd('narccap_rotated_pole',kwds=dict(time_region={'month':[12],'year':[1982]}))
-        calc = [{'func':'mean','name':'mean'}]
+        rd = self.test_data.get_rd('narccap_rotated_pole', kwds=dict(time_region={'month': [12], 'year': [1982]}))
+        calc = [{'func': 'mean', 'name': 'mean'}]
         calc_grouping = ['month']
-        ops = OcgOperations(dataset=rd,calc=calc,calc_grouping=calc_grouping,
+        ops = OcgOperations(dataset=rd, calc=calc, calc_grouping=calc_grouping,
                             output_format='nc')
         ret = ops.execute()
-        field = ocgis.RequestDataset(uri=ret,variable='mean').get()
-        self.assertIsInstance(field.spatial.crs,CFRotatedPole)
-        self.assertEqual(field.shape,(1,1,1,130,155))
-        
+        field = ocgis.RequestDataset(uri=ret, variable='mean').get()
+        self.assertIsInstance(field.spatial.crs, CFRotatedPole)
+        self.assertEqual(field.shape, (1, 1, 1, 130, 155))
+
     def test_intersects(self):
-        rd = self.test_data.get_rd('narccap_rotated_pole',kwds=dict(time_region={'month':[12],'year':[1982]}))
-        ops = OcgOperations(dataset=rd,geom='state_boundaries',select_ugid=[16])
+        rd = self.test_data.get_rd('narccap_rotated_pole', kwds=dict(time_region={'month': [12], 'year': [1982]}))
+        ops = OcgOperations(dataset=rd, geom='state_boundaries', select_ugid=[16])
         ret = ops.execute()
-        ref = ret.gvu(16,'tas')
-        self.assertEqual(ref.shape,(1, 248, 1, 7, 15))
+        ref = ret.gvu(16, 'tas')
+        self.assertEqual(ref.shape, (1, 248, 1, 7, 15))
         self.assertAlmostEqual(np.ma.mean(ref), 269.8304976396538)
-        self.assertNumpyAll(ref.mask.squeeze()[0,:,:],
-                            np.array([[True,True,True,True,False,False,False,False,False,False,False,False,False,False,False],
-                                      [True,True,True,True,False,False,False,False,False,False,False,False,False,False,False],
-                                      [True,True,True,True,False,False,False,False,False,False,False,False,False,False,True],
-                                      [False,False,False,False,False,False,False,False,False,False,False,False,False,False,True],
-                                      [False,False,False,False,False,False,False,False,False,False,False,False,False,False,True],
-                                      [True,False,False,False,False,False,False,False,False,False,False,False,False,False,True],
-                                      [True,False,False,False,False,False,False,False,False,False,True,True,True,True,True]],dtype=bool))
-        
+        self.assertNumpyAll(ref.mask.squeeze()[0, :, :],
+                            np.array([[True, True, True, True, False, False, False, False, False, False, False, False,
+                                       False, False, False],
+                                      [True, True, True, True, False, False, False, False, False, False, False, False,
+                                       False, False, False],
+                                      [True, True, True, True, False, False, False, False, False, False, False, False,
+                                       False, False, True],
+                                      [False, False, False, False, False, False, False, False, False, False, False,
+                                       False, False, False, True],
+                                      [False, False, False, False, False, False, False, False, False, False, False,
+                                       False, False, False, True],
+                                      [True, False, False, False, False, False, False, False, False, False, False,
+                                       False, False, False, True],
+                                      [True, False, False, False, False, False, False, False, False, False, True, True,
+                                       True, True, True]], dtype=bool))
+
     def test_clip_aggregate(self):
-        rd = self.test_data.get_rd('narccap_rotated_pole',kwds=dict(time_region={'month':[12],'year':[1982]}))
-        ops = OcgOperations(dataset=rd,geom='state_boundaries',select_ugid=[16],
-                            spatial_operation='clip',aggregate=True,output_format='numpy')
-        ## the output CRS should be automatically updated for this operation
-        self.assertEqual(ops.output_crs,CFWGS84())
+        rd = self.test_data.get_rd('narccap_rotated_pole', kwds=dict(time_region={'month': [12], 'year': [1982]}))
+        ops = OcgOperations(dataset=rd, geom='state_boundaries', select_ugid=[16],
+                            spatial_operation='clip', aggregate=True, output_format='numpy')
+        # the output CRS should be automatically updated for this operation
+        self.assertEqual(ops.output_crs, CFWGS84())
         ret = ops.execute()
-        ret = ret.gvu(16,'tas')
-        self.assertEqual(ret.shape,(1, 248, 1, 1, 1))
+        ret = ret.gvu(16, 'tas')
+        self.assertEqual(ret.shape, (1, 248, 1, 1, 1))
         self.assertAlmostEqual(ret.mean(), 269.83051915322579)
-        
+
     def test_read(self):
         rd = self.test_data.get_rd('narccap_rotated_pole')
         field = rd.get()
-        self.assertIsInstance(field.spatial.crs,CFRotatedPole)
-        
+        self.assertIsInstance(field.spatial.crs, CFRotatedPole)
+
     def test_to_netcdf(self):
-        rd = self.test_data.get_rd('narccap_rotated_pole',kwds=dict(time_region={'month':[12],'year':[1982]}))
-        ## it does not care about slices or no geometries
-        ops = OcgOperations(dataset=rd,output_format='nc')
+        rd = self.test_data.get_rd('narccap_rotated_pole', kwds=dict(time_region={'month': [12], 'year': [1982]}))
+        # it does not care about slices or no geometries
+        ops = OcgOperations(dataset=rd, output_format='nc')
         ret = ops.execute()
-        rd2 = ocgis.RequestDataset(uri=ret,variable='tas')
-        self.assertEqual(rd2.get().temporal.extent,(5444.0,5474.875))
-        
+        rd2 = ocgis.RequestDataset(uri=ret, variable='tas')
+        self.assertEqual(rd2.get().temporal.extent, (5444.0, 5474.875))
+
     def test_to_netcdf_with_geometry(self):
         rd = self.test_data.get_rd('narccap_rotated_pole')
         # this bounding box covers the entire spatial domain. the software will move between rotated pole and CFWGS84
@@ -93,25 +97,25 @@ class TestRotatedPole(TestBase):
                     var2 = ds2.variables[var_name][:]
                     diff = np.abs(var - var2)
                     self.assertTrue(diff.max() <= 1.02734374963e-06)
-        
+
     def test_to_netcdf_with_slice(self):
         rd = self.test_data.get_rd('narccap_rotated_pole')
         ops = OcgOperations(dataset=rd,
                             output_format='nc',
-                            slice=[None,[0,10],None,[0,10],[0,10]],
+                            slice=[None, [0, 10], None, [0, 10], [0, 10]],
                             prefix='slice')
         ret = ops.execute()
-        rd3 = ocgis.RequestDataset(uri=ret,variable='tas')
-        self.assertEqual(rd3.get().shape,(1,10,1,10,10))
-        
-        
+        rd3 = ocgis.RequestDataset(uri=ret, variable='tas')
+        self.assertEqual(rd3.get().shape, (1, 10, 1, 10, 10))
+
+
 class Test(TestBase):
-            
     def test_cf_lambert_conformal(self):
         rd = self.test_data.get_rd('narccap_lambert_conformal')
         field = rd.get()
         crs = field.spatial.crs
-        self.assertDictEqual(crs.value,{'lon_0': -97, 'ellps': 'WGS84', 'y_0': 2700000, 'no_defs': True, 'proj': 'lcc', 'x_0': 3325000, 'units': 'm', 'lat_2': 60, 'lat_1': 30, 'lat_0': 47.5})
+        self.assertDictEqual(crs.value, {'lon_0': -97, 'ellps': 'WGS84', 'y_0': 2700000, 'no_defs': True, 'proj': 'lcc',
+                                         'x_0': 3325000, 'units': 'm', 'lat_2': 60, 'lat_1': 30, 'lat_0': 47.5})
 
     @attr('slow')
     def test_read_write_projections(self):
@@ -120,7 +124,7 @@ class Test(TestBase):
         data_dir = os.path.join(ocgis.env.DIR_TEST_DATA, 'nc', 'narccap')
         ocgis.env.DIR_DATA = data_dir
         ocgis.env.OVERWRITE = True
-        
+
         real = {'pr': {'pr_RCM3_cgcm3_1986010103.nc': {'mu': 2.7800052478033606e-07, 'shape': (1, 1, 1, 7, 15)},
                        'pr_MM5I_ncep_1981010103.nc': {'mu': 3.3648159627007675e-08, 'shape': (1, 1, 1, 7, 14)},
                        'pr_RCM3_ncep_1986010103.nc': {'mu': 9.7176247154926553e-09, 'shape': (1, 1, 1, 7, 15)},
@@ -189,36 +193,33 @@ class Test(TestBase):
         for uri in os.listdir(data_dir):
             if uri.endswith('nc'):
                 variable = uri.split('_')[0]
-                for output_format in [
-                                      'numpy',
-                                      'nc'
-                                      ]:
-                    rd = RequestDataset(uri=uri,variable=variable)#,time_range=time_range)
-                    
+                for output_format in ['numpy', 'nc']:
+                    rd = RequestDataset(uri=uri, variable=variable)
+
                     try:
-                        ops = OcgOperations(dataset=rd,geom='state_boundaries',select_ugid=[16],snippet=True,
+                        ops = OcgOperations(dataset=rd, geom='state_boundaries', select_ugid=[16], snippet=True,
                                             output_format=output_format)
                     except DefinitionValidationError:
-                        ## rotated pole may not be written to netCDF
+                        # rotated pole may not be written to netCDF
                         crs = rd._get_crs_()
-                        if isinstance(crs,CFRotatedPole):
+                        if isinstance(crs, CFRotatedPole):
                             continue
                         else:
                             raise
-                    
+
                     try:
                         ret = ops.execute()
-                        
+
                         if output_format == 'numpy':
                             ref = ret[16].values()[0].variables.values()[0].value
                             mu = np.ma.mean(ref)
-                            
+
                             r_mu = real[variable][uri]['mu']
                             try:
-                                self.assertAlmostEqual(r_mu,mu)
+                                self.assertAlmostEqual(r_mu, mu, places=4)
                             except AssertionError:
-                                self.assertEqual(r_mu,np.ma.core.MaskedConstant)
-                                self.assertEqual(real[variable][uri]['shape'],ref.shape)
+                                self.assertEqual(r_mu, np.ma.core.MaskedConstant)
+                                self.assertEqual(real[variable][uri]['shape'], ref.shape)
 
                         if output_format == 'nc':
                             with nc_scope(ret) as ds:
@@ -226,16 +227,11 @@ class Test(TestBase):
                                     grid_mapping = ds.variables[rd.variable].grid_mapping
                                     self.assertTrue(grid_mapping in ds.variables)
                                 except AttributeError:
-                                    ## time slice files do not have a projection
+                                    # time slice files do not have a projection
                                     self.assertTrue('_TMSL_' in rd.uri)
-                                
+
                     except ExtentError:
                         if 'ECP2_ncep' in rd.uri:
                             pass
                         else:
                             raise
-
-
-if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
-    unittest.main()
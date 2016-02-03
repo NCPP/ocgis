1633ab7d7ede6a919ea4e81af6380d610441cdb4
bekozi <benkoziol@gmail.com>
2016-02-03 14:54:11 -0700

    FIX: __init__ commit

--- src/ocgis/__init__.py
+++ src/ocgis/__init__.py
@@ -1,22 +1,23 @@
-# @formatter:off
+from ocgis.util.environment import env
+import osgeo
+from osgeo import osr
+from osgeo import ogr
+from ocgis.interface.base.dimension.base import VectorDimension
+from ocgis.interface.base.dimension.temporal import TemporalDimension
 from ocgis.api.collection import SpatialCollection
-from ocgis.api.operations import OcgOperations
-from ocgis.api.request.base import RequestDataset, RequestDatasetCollection
 from ocgis.calc.library.register import FunctionRegistry
 from ocgis.interface.base import crs
 from ocgis.interface.base.crs import CoordinateReferenceSystem
-from ocgis.interface.base.dimension.base import VectorDimension
 from ocgis.interface.base.dimension.spatial import SpatialDimension, SpatialGridDimension, SpatialGeometryDimension, \
     SpatialGeometryPolygonDimension, SpatialGeometryPointDimension
-from ocgis.interface.base.dimension.temporal import TemporalDimension
 from ocgis.interface.base.field import Field
-from ocgis.interface.base.variable import Variable
-from ocgis.util.environment import env
-from ocgis.util.geom_cabinet import GeomCabinet, GeomCabinetIterator, ShpCabinet, ShpCabinetIterator
 from ocgis.util.inspect import Inspect
+from ocgis.util.geom_cabinet import GeomCabinet, GeomCabinetIterator, ShpCabinet, ShpCabinetIterator
 from ocgis.util.zipper import format_return
+from ocgis.interface.base.variable import Variable
+from ocgis.api.request.base import RequestDataset, RequestDatasetCollection
+from ocgis.api.operations import OcgOperations
 
 
 __version__ = '1.3.0.dev1'
 __release__ = '1.3.0.dev1'
-#@formatter:on

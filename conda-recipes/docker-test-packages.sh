63ce0f5143545aacb2db2816021d8a944c2b0ba4
bekozi <benkoziol@gmail.com>
2016-02-26 11:52:15 -0700

    WIP: Fix to Travis and RTD

--- conda-recipes/docker-test-packages.sh
+++ conda-recipes/docker-test-packages.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+docker pull bekozi/ntest-ubuntu
 docker rm ocgis-test-runner
 docker run -id --name ocgis-test-runner bekozi/ntest-ubuntu
-docker exec ocgis-test-runner bash -c "conda create -y -n ocgis -c nesii/channel/ocgis -c nesii ocgis nose esmpy"
-docker exec ocgis-test-runner bash -c "source activate ocgis && python -c \"from ocgis.test import run_all; run_all()\""
+docker exec ocgis-test-runner bash -c "conda create -y -n ocgis -c nesii/channel/ocgis -c nesii ocgis==1.2.1 nose"
+docker exec ocgis-test-runner bash -c "source activate ocgis && python -c \"from ocgis.test import run_simple; run_simple()\""
 docker stop ocgis-test-runner

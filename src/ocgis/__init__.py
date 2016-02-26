73ccc7249037481563aadc447a90be7af915d080
bekozi <benkoziol@gmail.com>
2016-02-26 12:11:33 -0700

    FIX: Revert for ocgis.__init__

--- src/ocgis/__init__.py
+++ src/ocgis/__init__.py
@@ -1,19 +1,22 @@
+from ocgis.util.environment import env
+import osgeo
+from osgeo import osr
+from osgeo import ogr
 from ocgis.api.collection import SpatialCollection
 from ocgis.api.operations import OcgOperations
 from ocgis.api.request.base import RequestDataset, RequestDatasetCollection
 from ocgis.calc.library.register import FunctionRegistry
 from ocgis.interface.base import crs
 from ocgis.interface.base.crs import CoordinateReferenceSystem
-from ocgis.interface.base.dimension.base import VectorDimension
 from ocgis.interface.base.dimension.spatial import SpatialDimension, SpatialGridDimension, SpatialGeometryDimension, \
     SpatialGeometryPolygonDimension, SpatialGeometryPointDimension
-from ocgis.interface.base.dimension.temporal import TemporalDimension
 from ocgis.interface.base.field import Field
-from ocgis.interface.base.variable import Variable
-from ocgis.util.environment import env
 from ocgis.util.inspect import Inspect
 from ocgis.util.shp_cabinet import ShpCabinet, ShpCabinetIterator
 from ocgis.util.zipper import format_return
+from ocgis.interface.base.dimension.temporal import TemporalDimension
+from ocgis.interface.base.variable import Variable
+from ocgis.interface.base.dimension.base import VectorDimension
 
 __version__ = '1.2.1'
 __release__ = '1.2.1'

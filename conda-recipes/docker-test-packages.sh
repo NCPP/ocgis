efe8d8738822f8ddf359db6b6edf9457289f8240
bekozi <benkoziol@gmail.com>
2016-03-02 09:13:44 -0700

    BLD: Returned conda recipes to conda-recipes-nesii

--- conda-recipes/docker-test-packages.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-docker pull bekozi/ntest-ubuntu
-docker rm ocgis-test-runner
-docker run -id --name ocgis-test-runner bekozi/ntest-ubuntu
-docker exec ocgis-test-runner bash -c "conda create -y -n ocgis -c nesii ocgis nose esmpy"
-docker exec ocgis-test-runner bash -c "source activate ocgis && python -c \"from ocgis.test import run_all; run_all()\""
-docker stop ocgis-test-runner
--- conda-recipes/gdal/run_test.py
+++
@@ -1,5 +0,0 @@
-import ogr
-
-cnt = ogr.GetDriverCount()
-for i in range(cnt):
-    print(ogr.GetDriver(i).GetName())

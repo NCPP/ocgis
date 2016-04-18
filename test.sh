01def1f30fc8d57543d2a71a31f6a5e3e01b8bd3
bekozi <benkoziol@gmail.com>
2016-04-18 14:03:04 -0600

    BLD: Incremented version

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@
 #export OCGIS_DIR_GEOMCABINET=/home/ubuntu/data/ocgis_test_data/shp
 
 rm .noseids
-nosetests -vs --with-id -a '!remote' src/ocgis/test 2>&1 | tee /tmp/test_ocgis.out
+nosetests -vs --with-id -a '!remote' src/ocgis/test 2>&1 | tee ./test_ocgis.out

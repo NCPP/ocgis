49c09847dab26f197ab09596936040cc8d1b8677
bekozi <benkoziol@gmail.com>
2017-01-27 08:43:09 -0700

    TST: Multiple time units for MFDatasets #435
    Added MFTime unit test for the netCDF4-python library

--- src/ocgis/test/test_simple/test_dependencies.py
+++ src/ocgis/test/test_simple/test_dependencies.py
@@ -1,5 +1,8 @@
 import os
 
+import numpy as np
+from netCDF4._netCDF4 import MFDataset, MFTime
+
 from ocgis import CoordinateReferenceSystem
 from ocgis.test.base import TestBase, attr
 
@@ -17,6 +20,32 @@ class TestDependencies(TestBase):
         with self.nc_scope(path) as ds:
             var = ds.variables['foo']
             self.assertEqual(var.shape, (1, 1))
+
+    def test_netCDF4_MFTime(self):
+        value = [0, 1, 2]
+        units = ['days since 2000-1-1', 'days since 2001-1-1']
+        names = ['time_2000.nc', 'time_0001.nc']
+        paths = []
+
+        for unit, name in zip(units, names):
+            path = self.get_temporary_file_path(name)
+            paths.append(path)
+
+            with self.nc_scope(path, 'w', format='NETCDF4_CLASSIC') as f:
+                f.createDimension('time')
+                vtime = f.createVariable('time', np.float32, dimensions=('time',))
+                vtime[:] = value
+                vtime.calendar = 'standard'
+                vtime.units = unit
+
+        mfd = MFDataset(paths)
+        try:
+            mvtime = MFTime(mfd.variables['time'])
+            desired = [0., 1., 2., 366., 367., 368.]
+            actual = mvtime[:].tolist()
+            self.assertEqual(actual, desired)
+        finally:
+            mfd.close()
 
     def test_osr(self):
         crs = CoordinateReferenceSystem(epsg=4326)

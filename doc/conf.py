cefc0165a937472c0a77752f9896a755dc4b4eab
bekozi <benkoziol@gmail.com>
2017-06-29 12:42:11 -0600

    FIX: Removed Sphinx dependency for RTD

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,8 @@ sys.path.insert(0,os.path.abspath('../src'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.pngmath', 'sphinx.ext.mathjax', 'sphinx.ext.intersphinx', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.imgmath', 'sphinx.ext.intersphinx',
+              'sphinx.ext.viewcode']
 intersphinx_mapping = {'python': ('http://docs.python.org/2.7', None)}
 
 # Add any paths that contain templates here, relative to this directory.

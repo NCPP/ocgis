99539e46e1e269aa91ebcd32105a0c23a36b3d38
bekozi <benkoziol@gmail.com>
2018-01-04 09:53:35 -0700

    CLN: Updated copyright

--- doc/conf.py
+++ doc/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'OpenClimateGIS'
-copyright = (u'2013, University of Michigan-Ann Arbor, Michigan Technological University, University of Colorado-Boulder')
+copyright = (u'2012-2018, University of Michigan-Ann Arbor, Michigan Technological University, University of Colorado-Boulder')
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
